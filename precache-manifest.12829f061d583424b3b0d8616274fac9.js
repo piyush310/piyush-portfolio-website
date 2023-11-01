56897664a1b09e2d3d8fe38ab0e139a8de5bb5da
Piyush Shukla <piyushshukla356@gmail.com>
2023-11-01 18:12:09 +0530

    Updates

---
+++ precache-manifest.12829f061d583424b3b0d8616274fac9.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e913b782848d2723841ff194e6ac0069",
+    "url": "/piyush-portfolio-website/index.html"
+  },
+  {
+    "revision": "cbac21dc1591271a8671",
+    "url": "/piyush-portfolio-website/static/css/3.ca5647f4.chunk.css"
+  },
+  {
+    "revision": "ff4394531672d4258fc4",
+    "url": "/piyush-portfolio-website/static/css/4.12c5d646.chunk.css"
+  },
+  {
+    "revision": "2872fb0722b1cf33218e",
+    "url": "/piyush-portfolio-website/static/css/main.1908cc93.chunk.css"
+  },
+  {
+    "revision": "56a9ebe63781b589c95f",
+    "url": "/piyush-portfolio-website/static/js/2.753ba6f9.chunk.js"
+  },
+  {
+    "revision": "408f8ade359af8695a328ed937291fdf",
+    "url": "/piyush-portfolio-website/static/js/2.753ba6f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbac21dc1591271a8671",
+    "url": "/piyush-portfolio-website/static/js/3.06928a0e.chunk.js"
+  },
+  {
+    "revision": "ff4394531672d4258fc4",
+    "url": "/piyush-portfolio-website/static/js/4.d87367ab.chunk.js"
+  },
+  {
+    "revision": "2872fb0722b1cf33218e",
+    "url": "/piyush-portfolio-website/static/js/main.db70bd30.chunk.js"
+  },
+  {
+    "revision": "d3041715986ab568d87d",
+    "url": "/piyush-portfolio-website/static/js/runtime-main.ca1e1b71.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/piyush-portfolio-website/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/piyush-portfolio-website/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/piyush-portfolio-website/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/piyush-portfolio-website/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "b7ae5c02da6b83c76d8fa436f2010b34",
+    "url": "/piyush-portfolio-website/static/media/gcp.b7ae5c02.png"
+  },
+  {
+    "revision": "31c1923651927c6b98d184f1517858c2",
+    "url": "/piyush-portfolio-website/static/media/hack.31c19236.jpeg"
+  },
+  {
+    "revision": "f660ed232cc3bd3c44fe7375eeb01af3",
+    "url": "/piyush-portfolio-website/static/media/iiitbh.f660ed23.png"
+  },
+  {
+    "revision": "2ff99bcb1a03586a9b69cff3c7928e81",
+    "url": "/piyush-portfolio-website/static/media/iit.2ff99bcb.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/piyush-portfolio-website/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "0e2d23b475a04ed8e5e2000f7f445ca4",
+    "url": "/piyush-portfolio-website/static/media/quantiphilogo.0e2d23b4.png"
+  },
+  {
+    "revision": "46233b00527fab184fd837dca42592c9",
+    "url": "/piyush-portfolio-website/static/media/recog.46233b00.jpeg"
+  },
+  {
+    "revision": "3c3ce1b1ab6691f1bf7f952674fbcb66",
+    "url": "/piyush-portfolio-website/static/media/red.3c3ce1b1.png"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/piyush-portfolio-website/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/piyush-portfolio-website/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "e8dece55f90fb9f6282c59d1bb39a19d",
+    "url": "/piyush-portfolio-website/static/media/vss.e8dece55.jpeg"
+  }
+]);
